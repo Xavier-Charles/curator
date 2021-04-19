ef9f753938421888fa182e6dba08bcd1c09348bd
charlesnzubechukwu000@gmail.com <charlesnzubechukwu000@gmail.com>
2021-04-19 17:28:25 +0100

    Used Cards and stateful widgets

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,15 @@ void main() {
   ));
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var title = "Change My Name";
+  TextEditingController _titleController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,47 +25,72 @@ class HomePage extends StatelessWidget {
         title: Text("Curator"),
       ),
       body: Center(
-        child: Container(
-          color: Colors.teal[400],
-          width: 100,
-          height: 100,
-        ),
-      ),
+          child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: SingleChildScrollView(
+                  child: Card(
+                      child: Column(
+                children: <Widget>[
+                  Image.asset("assets/omar-Sneakers.jpg"),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    title,
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: TextField(
+                      controller: _titleController,
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: "Enter Product name",
+                          labelText: "Name"),
+                    ),
+                  )
+                ],
+              ))))),
       drawer: Drawer(
-        child: ListView(
-          padding: const EdgeInsets.all(0),
-          children: <Widget>[
-            // DrawerHeader(
-            //   child: Text("Hi"),
-            //   decoration: BoxDecoration(color: Colors.amber[300]),
-            //   ),
-            UserAccountsDrawerHeader(
-              accountName: Text("Xharles Dev"), 
-              accountEmail: Text("Hello@Xharles.dev"),
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: NetworkImage("https://images.unsplash.com/photo-1522529599102-193c0d76b5b6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"),
-              ),
-              ),
-            ListTile(
-              leading: Icon(Icons.person),
-              title: Text("Xharles Dev"),
-              subtitle: Text("Flutter is on fire"),
-              trailing: Icon(Icons.edit),
-              onTap: () => {},
+          child: ListView(
+        padding: const EdgeInsets.all(0),
+        children: <Widget>[
+          // DrawerHeader(
+          //   child: Text("Hi"),
+          //   decoration: BoxDecoration(color: Colors.amber[300]),
+          //   ),
+          UserAccountsDrawerHeader(
+            accountName: Text("Xharles Dev"),
+            accountEmail: Text("Hello@Xharles.dev"),
+            currentAccountPicture: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  "https://images.unsplash.com/photo-1522529599102-193c0d76b5b6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"),
             ),
-            ListTile(
+          ),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text("Xharles Dev"),
+            subtitle: Text("Flutter is on fire"),
+            trailing: Icon(Icons.edit),
+            onTap: () => {},
+          ),
+          ListTile(
               leading: Icon(Icons.email),
               title: Text("Email"),
               subtitle: Text("Hello@Xharles.dev"),
-              trailing: Icon(Icons.edit)
-            ),
-          ],
-        )
-      ),
+              trailing: Icon(Icons.edit)),
+        ],
+      )),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => {},
-        child: Icon(Icons.edit),
-        ),
+        onPressed: () => {
+          title = _titleController.text,
+          setState(() => {})
+        },
+        child: Icon(Icons.send),
+      ),
     );
   }
 }
