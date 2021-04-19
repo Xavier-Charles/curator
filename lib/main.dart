e158688dacb99cec0fcd29cc8435775987caa4d3
charlesnzubechukwu000@gmail.com <charlesnzubechukwu000@gmail.com>
2021-04-19 15:26:44 +0100

    messing with rows and columns

--- lib/main.dart
+++ lib/main.dart
@@ -16,32 +16,34 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Curator"),
       ),
-      body: Center(
-        child: Container(
-        width: 120,
-        height: 120,
-        alignment: Alignment.topLeft,
-        decoration: BoxDecoration(
-          color: Colors.red[400],
-          borderRadius: BorderRadius.circular(10),
-          gradient: LinearGradient(colors: [
-            Colors.pink[300],
-            Colors.red[600]
-          ]),
-          boxShadow: [BoxShadow(
-            color: Colors.grey[350],
-            blurRadius:10,
-          )]
-        ),
-        child: Text("Enter the Gates of the Lord",
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            fontSize: 20,
-            color: Colors.white70,
-            fontWeight: FontWeight.bold 
-          ),
-        ),
-      ),
+      body: Container(
+        color: Colors.teal[200],
+        width: 300,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          // mainAxisSize: MainAxisSize.max, doesn't
+          children: <Widget>[
+            Container(
+              color: Colors.green[400],
+              width: 90,
+              height: 120,
+              alignment: Alignment.topLeft,        
+            ),
+            Container(
+              color: Colors.red[400],
+              width: 90,
+              height: 120,
+              alignment: Alignment.topLeft,        
+            ),
+            Container(
+              color: Colors.blue[400],
+              width: 90,
+              height: 120,
+              alignment: Alignment.topLeft,        
+            ),
+          ],
+        )
       )
       
     );
