83e435d0979abc6c8840f45253dac88d5101d224
charlesnzubechukwu000@gmail.com <charlesnzubechukwu000@gmail.com>
2021-04-20 15:36:23 +0100

    Consuming an api using HTTP

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:curator_app/drawer.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-import '../product_card_widget.dart';
-
+// import '../product_card_widget.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -10,8 +11,30 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  var title = "Change My Name";
-  TextEditingController _titleController = TextEditingController();
+  // var title = "Change My Name";
+  // TextEditingController _titleController = TextEditingController();
+
+  var url = "https://jsonplaceholder.typicode.com/photos";
+  var data;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  fetchData() async {
+    var res = await http.get(Uri.parse(url));
+    print(res.body);
+    data = jsonDecode(res.body);
+    setState(() => {});
+    print(data);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,18 +42,23 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text("Curator"),
       ),
-      body: Center(
-          child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: SingleChildScrollView(
-                  child: ProductCardWidget(title: title, titleController: _titleController)
-              ))),
+      body: data != null
+          ? ListView.builder(
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(data[index]["title"]),
+                  subtitle: Text("ID: ${data[index]["id"]}"),
+                  leading: Image.network(data[index]["url"])
+                );
+              },
+              itemCount: data.length,
+            )
+          : Center(
+              child: CircularProgressIndicator(),
+            ),
       drawer: MyDrawer(),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => {
-          title = _titleController.text,
-          setState(() => {})
-        },
+        onPressed: () => {},
         child: Icon(Icons.send),
       ),
     );
